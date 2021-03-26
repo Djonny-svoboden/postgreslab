e91ce7cdc5e7f2aab671db367d92cfc2490c3ada
Pikabu <57330062+Djonny-svoboden@users.noreply.github.com>
2021-03-26 15:39:51 +0200

    Add files via upload

---
+++ lab3/lab3.sql
@@ -0,0 +1,15 @@
+1#######################
+SELECT customers.first_name, customers.last_name, products.id
+FROM customers
+INNER JOIN products ON customers.basket_id=products.id
+;	
+2########################
+SELECT * from customers 
+where payment < 50 AND city not in('Batallas','Plavsk') AND id >15
+;
+3#########################by payment	
+select customers.first_name, customers.last_name, basket.cost
+from customers
+inner join basket on customers.payment = basket.cost
+;
+4###########################
